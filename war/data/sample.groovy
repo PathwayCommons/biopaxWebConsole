a227316d6f6719e97041752b6a0fda69fd4aa4c1
cannin <cannin@gmail.com>
2015-06-23 22:48:14 -0400

    Added sample files Minor changes to layouts

---
+++ war/data/sample.groovy
@@ -0,0 +1,20 @@
+import org.biopax.paxtools.io.SimpleIOHandler;
+import org.biopax.paxtools.io.BioPAXIOHandler;
+import org.biopax.paxtools.model.Model;
+import org.biopax.paxtools.model.level3.*;
+ 
+InputStream f = new FileInputStream(new File("./data/test_biopax.owl"));
+ 
+BioPAXIOHandler handler = new SimpleIOHandler();
+Model rawModel = handler.convertFromOWL(f);
+ 
+f.close();
+
+// Get all BiochemicalReactions
+Set<BiochemicalReaction> rawModelObjects = rawModel.getObjects(BiochemicalReaction.class);
+
+// Print participants in BiochemicalReactions
+for (BiochemicalReaction rawModelObject : rawModelObjects) {
+  String tmp = "BiochemicalReaction: " + rawModelObject.getRDFId();
+  System.out.println(tmp);
+}
